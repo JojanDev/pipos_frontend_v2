19aa48dff0d5c36c3f12f7fac87888cd31ad7bb6
Johan Sebastian Delgado Mantilla <zzaibor69@gmail.com>
2025-07-29 22:38:19 -0500

    Migración de tabla a Grid

--- src/helpers/crearFila.js
+++ src/helpers/crearFila.js
@@ -1,11 +1,11 @@
 export const crearFila = (datos = []) => {
   const fila = document.createElement("tr");
-  fila.classList.add("table__row-body");
+  fila.classList.add("table__row");
   fila.setAttribute("data-id", datos[0]);
 
   datos.forEach((dato) => {
     const celda = document.createElement("td");
-    celda.classList.add("table__cell-body");
+    celda.classList.add("table__cell");
     celda.textContent = dato;
     fila.appendChild(celda);
   });
--- src/views/clients/clientsController.js
+++ src/views/clients/clientsController.js
@@ -1,40 +1,6 @@
 import { get } from "../../helpers/api.js";
 import { cargarTiposDocumento } from "../../helpers/cargarTiposDocumento.js";
-
-const crearCliente = ({
-  id,
-  info: { correo, direccion, numeroDocumento, nombre, telefono },
-}) => {
-  const tr = document.createElement("tr");
-
-  const tdId = document.createElement("td");
-  const tdCliente = document.createElement("td");
-  const tdDocumento = document.createElement("td");
-  const tdDireccion = document.createElement("td");
-  const tdTelefono = document.createElement("td");
-  const tdEmail = document.createElement("td");
-
-  tr.setAttribute("data-id", id);
-
-  tdId.textContent = id;
-  tdCliente.textContent = nombre;
-  tdDireccion.textContent = direccion;
-  tdDocumento.textContent = numeroDocumento;
-  tdTelefono.textContent = telefono;
-  tdEmail.textContent = correo;
-
-  tr.classList.add("table__row-body");
-
-  tdId.classList.add("table__cell-body");
-  tdCliente.classList.add("table__cell-body");
-  tdTelefono.classList.add("table__cell-body");
-  tdDocumento.classList.add("table__cell-body");
-  tdDireccion.classList.add("table__cell-body");
-  tdEmail.classList.add("table__cell-body");
-
-  tr.append(tdId, tdCliente, tdTelefono, tdDocumento, tdDireccion, tdEmail);
-  return tr;
-};
+import { crearFila } from "../../helpers/crearFila.js";
 
 const cargarTabla = async () => {
   const clientes = await get("clientes");
@@ -45,10 +11,27 @@ const cargarTabla = async () => {
     await error(response.message);
   }
 
-  const tbody = document.querySelector(".table__body");
+  const tbody = document.querySelector("#clients .table__body");
 
-  clientes.data.forEach((cliente) => {
-    const row = crearCliente(cliente);
+  const clientesInfo = clientes.data.map((cliente) => {
+    // console.log("cliente", cliente);
+
+    const info = cliente.info;
+    // console.log("info", info);
+
+    return [
+      cliente.id,
+      info.nombre,
+      info.telefono,
+      info.numeroDocumento,
+      info.direccion,
+    ];
+  });
+
+  console.log(clientesInfo);
+
+  clientesInfo.forEach((cliente) => {
+    const row = crearFila(cliente);
     tbody.append(row);
   });
 };
@@ -56,7 +39,7 @@ const cargarTabla = async () => {
 export const clientsController = async () => {
   cargarTabla();
 
-  const tablaClientes = document.querySelector("#clientes");
+  const tablaClientes = document.querySelector("#clients");
 
   tablaClientes.addEventListener("click", (event) => {
     const fila = event.target.closest("tr[data-id]");
